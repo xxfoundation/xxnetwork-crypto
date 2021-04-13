56bd15ec9d993832bb338b4709029830a99b03bd
josh <josh@elixxir.io>
2021-04-13 13:09:52 -0700

    Remove UnmarshalEllipticPublicKey

--- signature/ec/ec.go
+++ signature/ec/ec.go
@@ -11,34 +11,13 @@ package ec
 import (
 	"crypto/rand"
 	"encoding/pem"
-	"fmt"
 	"github.com/katzenpost/core/crypto/eddsa"
-	"github.com/pkg/errors"
 )
 
 const (
 	privateKeyType = "ED25519 PRIVATE KEY"
 	publicKeyType  = "ED25519 PUBLIC KEY"
 )
-
-// UnmarshalEllipticPublicKey generates an elliptic curve key pair.
-// Then using the eddsa library to deserialize publicKeyStr into an
-// EC public key
-func UnmarshalEllipticPublicKey(publicKeyStr string) (*eddsa.PublicKey, error) {
-	// Generate a key to unmarshal
-	ecKey, err := eddsa.NewKeypair(rand.Reader)
-	if err != nil {
-		return nil, errors.WithMessage(err, fmt.Sprintf("Could not generate an EC key"))
-	}
-
-	ecPublicKey := ecKey.PublicKey()
-	if err = ecPublicKey.FromString(publicKeyStr); err != nil {
-		return nil, errors.WithMessage(err, fmt.Sprint("Could not parse public key string"))
-	}
-
-	return ecPublicKey, nil
-
-}
 
 // CreatePrivateKeyPem creates a PEM file from a private key
 func CreatePublicKeyPem(key *eddsa.PublicKey) []byte {
--- signature/ec/ec_test.go
+++ signature/ec/ec_test.go
@@ -29,39 +29,6 @@ zpYsF/OHmo5qeOSGfkk8NHVJfLibWcAyg7h1V+maRs4=
 )
 
 // Happy path
-func TestUnmarshalEllipticPublicKey(t *testing.T) {
-	// Create a mock key
-	key, err := eddsa.NewKeypair(rand.Reader)
-	if err != nil {
-		t.Fatalf("Failed to create a test key: %v", err)
-	}
-
-	expectedPublicKey := key.PublicKey()
-
-	// Pass the marshalled public key into a string
-	publicKey, err := UnmarshalEllipticPublicKey(expectedPublicKey.String())
-	if err != nil {
-		t.Fatalf("Could not unmarshal key: %v", err)
-	}
-
-	if !reflect.DeepEqual(publicKey, expectedPublicKey) {
-		t.Fatalf("Unmarshalled public key did not match expected resutl."+
-			"\n\tExpected: %v"+
-			"\n\tReceived: %v", expectedPublicKey, publicKey)
-	}
-
-}
-
-// Error path
-func TestUnmarshalEllipticPublicKey_Error(t *testing.T) {
-	notValid := "notValid"
-	_, err := UnmarshalEllipticPublicKey(notValid)
-	if err == nil {
-		t.Fatalf("Expected error path: Key [%s] should not be a valid, parseable key", notValid)
-	}
-}
-
-// Happy path
 func TestCreatePrivateKeyPem(t *testing.T) {
 	decoded, err := base64.StdEncoding.DecodeString(privKeyEncoded)
 	if err != nil {
