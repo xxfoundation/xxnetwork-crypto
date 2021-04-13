dbc1dd97ed5ee9e2204caf08dcce245196d279b8
josh <josh@elixxir.io>
2021-04-13 11:46:28 -0700

    Add helper functions to elliptic key library

--- signature/ec/ec.go
+++ signature/ec/ec.go
@@ -10,9 +10,15 @@ package ec
 
 import (
 	"crypto/rand"
+	"encoding/pem"
 	"fmt"
 	"github.com/katzenpost/core/crypto/eddsa"
 	"github.com/pkg/errors"
+)
+
+const (
+	privateKeyType = "ED25519 PRIVATE KEY"
+	publicKeyType  = "ED25519 PUBLIC KEY"
 )
 
 // UnmarshalEllipticPublicKey generates an elliptic curve key pair.
@@ -32,4 +38,69 @@ func UnmarshalEllipticPublicKey(publicKeyStr string) (*eddsa.PublicKey, error) {
 
 	return ecPublicKey, nil
 
+}
+
+// CreatePrivateKeyPem creates a PEM file from a private key
+func CreatePublicKeyPem(key *eddsa.PublicKey) []byte {
+	block := &pem.Block{
+		Type:  publicKeyType,
+		Bytes: key.Bytes(),
+	}
+	pemBytes := pem.EncodeToMemory(block)
+	return pemBytes[:len(pemBytes)-1] // Strip newline
+}
+
+// CreatePrivateKeyPem creates a PEM file from a private key
+func CreatePrivateKeyPem(key *eddsa.PrivateKey) []byte {
+	block := &pem.Block{
+		Type:  privateKeyType,
+		Bytes: key.Bytes(),
+	}
+	pemBytes := pem.EncodeToMemory(block)
+	return pemBytes[:len(pemBytes)-1] // Strip newline
+}
+
+// LoadPrivateKeyFromByes decodes and produces an Eddsa PrivateKey
+func LoadPrivateKeyFromByes(pemBytes []byte) (*eddsa.PrivateKey, error) {
+	pk, err := eddsa.NewKeypair(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = pk.FromBytes(pemBytes); err != nil {
+		return nil, err
+	}
+	return pk, nil
+}
+
+// LoadPublicKeyFromString decodes and produces an Eddsa PublicKey from passed in string
+func LoadPublicKeyFromString(pemBytes string) (*eddsa.PublicKey, error) {
+	pk, err := eddsa.NewKeypair(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKey := pk.PublicKey()
+
+	if err = pubKey.FromString(pemBytes); err != nil {
+		return nil, err
+	}
+
+	return pubKey, nil
+}
+
+// LoadPublicKeyFromBytes decodes and produces an Eddsa PublicKey from passed in bytes
+func LoadPublicKeyFromBytes(pemBytes []byte) (*eddsa.PublicKey, error) {
+	pk, err := eddsa.NewKeypair(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKey := pk.PublicKey()
+
+	if err = pubKey.FromBytes(pemBytes); err != nil {
+		return nil, err
+	}
+
+	return pubKey, nil
 }
--- signature/ec/ec_test.go
+++ signature/ec/ec_test.go
@@ -7,10 +7,25 @@
 package ec
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/base64"
 	"github.com/katzenpost/core/crypto/eddsa"
 	"reflect"
 	"testing"
+)
+
+const (
+	privKeyEncoded = `uVAt6d+y3XW699L3THlcoTA2utw2dhoqnX6821x6OcnOliwX84eajmp45IZ+STw0dUl8uJtZwDKDuHVX6ZpGzg==`
+	privKeyPem     = `-----BEGIN ED25519 PRIVATE KEY-----
+uVAt6d+y3XW699L3THlcoTA2utw2dhoqnX6821x6OcnOliwX84eajmp45IZ+STw0
+dUl8uJtZwDKDuHVX6ZpGzg==
+-----END ED25519 PRIVATE KEY-----`
+	pubKeyEncoded = `zpYsF/OHmo5qeOSGfkk8NHVJfLibWcAyg7h1V+maRs4=`
+	pubKeyPem     = `-----BEGIN ED25519 PUBLIC KEY-----
+zpYsF/OHmo5qeOSGfkk8NHVJfLibWcAyg7h1V+maRs4=
+-----END ED25519 PUBLIC KEY-----`
+	pubKeyStr = `zpYsF/OHmo5qeOSGfkk8NHVJfLibWcAyg7h1V+maRs4=`
 )
 
 // Happy path
@@ -30,8 +45,8 @@ func TestUnmarshalEllipticPublicKey(t *testing.T) {
 	}
 
 	if !reflect.DeepEqual(publicKey, expectedPublicKey) {
-		t.Fatalf("Unmarshalled public key did not match expected resutl." +
-			"\n\tExpected: %v" +
+		t.Fatalf("Unmarshalled public key did not match expected resutl."+
+			"\n\tExpected: %v"+
 			"\n\tReceived: %v", expectedPublicKey, publicKey)
 	}
 
@@ -44,5 +59,105 @@ func TestUnmarshalEllipticPublicKey_Error(t *testing.T) {
 	if err == nil {
 		t.Fatalf("Expected error path: Key [%s] should not be a valid, parseable key", notValid)
 	}
+}
 
+// Happy path
+func TestCreatePrivateKeyPem(t *testing.T) {
+	decoded, err := base64.StdEncoding.DecodeString(privKeyEncoded)
+	if err != nil {
+		t.Fatalf("Failed to parse pem str: %v", err)
+	}
+
+	pk, err := LoadPrivateKeyFromByes(decoded)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	expected := []byte(privKeyPem)
+
+	pemOut := CreatePrivateKeyPem(pk)
+	if bytes.Compare(pemOut, expected) != 0 {
+		t.Errorf("Private Key Mismatch:"+
+			"\n\tExpected: %v"+
+			"\n\tReceived: %v",
+			expected, pemOut)
+	}
+}
+
+// Error path
+func TestLoadPrivateKeyFromByes_Error(t *testing.T) {
+	_, err := LoadPrivateKeyFromByes([]byte("invalid"))
+	if err == nil {
+		t.Errorf("Expected error path, should be invalid eddsa key")
+	}
+
+}
+
+// Error path
+func TestLoadPublicKeyFromByes_Error(t *testing.T) {
+	_, err := LoadPublicKeyFromBytes([]byte("invalid"))
+	if err == nil {
+		t.Errorf("Expected error path, should be invalid eddsa key")
+	}
+}
+
+// Happy path
+func TestCreatePublicKeyPem(t *testing.T) {
+	decoded, err := base64.StdEncoding.DecodeString(pubKeyEncoded)
+	if err != nil {
+		t.Fatalf("Failed to parse pem str: %v", err)
+	}
+
+	pubKey, err := LoadPublicKeyFromBytes(decoded)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	received := CreatePublicKeyPem(pubKey)
+	if err != nil {
+		t.Fatalf("Failed to load public key: %v", err)
+	}
+
+	expected := []byte(pubKeyPem)
+	if bytes.Compare(received, expected) != 0 {
+		t.Errorf("Private Key Mismatch:"+
+			"\n\tExpected: %v"+
+			"\n\tReceived: %v",
+			expected, received)
+	}
+
+}
+
+// Happy path
+func TestLoadPublicKeyFromString(t *testing.T) {
+	pk, err := eddsa.NewKeypair(rand.Reader)
+	if err != nil {
+		t.Fatalf("Failed to create a test key: %v", err)
+	}
+
+	expected := pk.PublicKey()
+	err = expected.FromString(pubKeyStr)
+	if err != nil {
+		t.Fatalf("Failed to load public key string into object: %v", err)
+	}
+
+	received, err := LoadPublicKeyFromString(pubKeyStr)
+	if err != nil {
+		t.Fatalf("Failed to load public key from string: %v", err)
+	}
+
+	if !reflect.DeepEqual(received, expected) {
+		t.Fatalf("Unexpected key mismatch."+
+			"\n\tExpected: %v"+
+			"\n\tReceived: %v", expected, received)
+	}
+
+}
+
+// Error path
+func TestLoadPublicKeyFromString_Error(t *testing.T) {
+	_, err := LoadPublicKeyFromString("invalid")
+	if err == nil {
+		t.Errorf("Expected error path, should be invalid eddsa key")
+	}
 }
