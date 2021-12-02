8a05abbb0b6c3395cf021250f06052a629ba618c
josh <josh@elixxir.io>
2021-12-02 15:21:44 -0800

    Define TLS ciphersuites

--- tls/credentials.go
+++ tls/credentials.go
@@ -5,11 +5,12 @@
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 // Package tls contains wrapper functions for creating GRPC credentials.
-// It also implements RSA key parsing
+// It also implements RSA key parsing.
 package tls
 
 import (
 	gorsa "crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
@@ -21,23 +22,16 @@ import (
 	"strings"
 )
 
-// getFullPath is a helper method which resolves ~ used in relative paths
-func getFullPath(path string) string {
-	if len(path) > 0 && path[0] == '~' {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			jww.ERROR.Printf("Unable to locate home directory: %v", err)
-		}
-		// Append the home directory to the path
-		return home + strings.TrimLeft(path, "~")
-	}
-	return path
+// acceptedCipherSuites is the accepted TLS cipher suites accepted by
+// the network.
+var acceptedCipherSuites = []uint16{
+	tls.TLS_CHACHA20_POLY1305_SHA256,
 }
 
-// NewCredentialsFromPEM creates a TransportCredentials object out of a string
-// Accepts a nameOverride for use in test environments
-func NewCredentialsFromPEM(certificate string, nameOverride string) (credentials.TransportCredentials, error) {
+// NewCredentialsFromPEM creates a TransportCredentials object out of a string.
+// Accepts a nameOverride for use in test environments.
+func NewCredentialsFromPEM(certificate string,
+	nameOverride string) (credentials.TransportCredentials, error) {
 	if nameOverride == "" {
 		jww.WARN.Printf("Failure to provide name override can result in" +
 			" TLS connection timeouts")
@@ -48,33 +42,52 @@ func NewCredentialsFromPEM(certificate string, nameOverride string) (credentials
 		jww.ERROR.Printf("Error appending certs to cert pool: %+v", certificate)
 		return nil, errors.New("failed to append cert to pool")
 	}
-	return credentials.NewClientTLSFromCert(pool, nameOverride), nil
+	return credentials.NewTLS(&tls.Config{
+		ServerName:   nameOverride,
+		RootCAs:      pool,
+		CipherSuites: acceptedCipherSuites,
+	}), nil
 }
 
-// NewCredentialsFromFile creates a TransportCredentials object from the contents of a file
-// Accepts a nameOverride for use in test environments
-func NewCredentialsFromFile(filePath string, nameOverride string) (credentials.TransportCredentials, error) {
+// NewCredentialsFromFile creates a TransportCredentials object
+// from the contents of a file. Accepts a nameOverride for use in
+// test environments.
+func NewCredentialsFromFile(filePath string,
+	nameOverride string) (credentials.TransportCredentials, error) {
 	if nameOverride == "" {
 		jww.WARN.Printf("Failure to provide name override can result in" +
 			" TLS connection timeouts")
 	}
 
+	// Read file
 	filePath = getFullPath(filePath)
-	result, err := credentials.NewClientTLSFromFile(filePath, nameOverride)
+	b, err := utils.ReadFile(filePath)
 	if err != nil {
-		jww.ERROR.Printf("Could not load TLS keys: %s", err)
 		return nil, err
 	}
-	return result, nil
+
+	// Construct cert pool
+	cp := x509.NewCertPool()
+	if !cp.AppendCertsFromPEM(b) {
+		return nil, errors.New("credentials: failed to append certificates")
+	}
+
+	return credentials.NewTLS(&tls.Config{
+		ServerName:   nameOverride,
+		RootCAs:      cp,
+		CipherSuites: acceptedCipherSuites,
+	}), nil
 }
 
-// NewPublicKeyFromFile reads the contents of a file and uses it to create a PublicKey object
+// NewPublicKeyFromFile reads the contents of a file and uses it
+// to create a PublicKey object.
 func NewPublicKeyFromFile(filePath string) (*rsa.PublicKey, error) {
 	//Pull the cert from the file
 	filePath = getFullPath(filePath)
 	certBytes, err := utils.ReadFile(filePath)
 	if err != nil {
-		jww.ERROR.Printf("Failed to read public key file at %s: %+v", filePath, err)
+		jww.ERROR.Printf("Failed to read public key file at %s: %+v",
+			filePath, err)
 		return nil, err
 	}
 
@@ -97,7 +110,7 @@ func NewPublicKeyFromFile(filePath string) (*rsa.PublicKey, error) {
 }
 
 // NewPublicKeyFromPEM accepts a PEM certificate block in []byte format
-// and returns a *rsa.PublicKey object
+// and returns a *rsa.PublicKey object.
 func NewPublicKeyFromPEM(certPEMblock []byte) (*rsa.PublicKey, error) {
 	block, _ := pem.Decode(certPEMblock)
 
@@ -114,4 +127,18 @@ func NewPublicKeyFromPEM(certPEMblock []byte) (*rsa.PublicKey, error) {
 	return &rsa.PublicKey{
 		PublicKey: *rsaPublicKey,
 	}, nil
+}
+
+// getFullPath is a helper method which resolves ~ used in relative paths
+func getFullPath(path string) string {
+	if len(path) > 0 && path[0] == '~' {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			jww.ERROR.Printf("Unable to locate home directory: %v", err)
+		}
+		// Append the home directory to the path
+		return home + strings.TrimLeft(path, "~")
+	}
+	return path
 }
