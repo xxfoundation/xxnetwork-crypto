69aad094b487900151b5ddc061a27f0aff76ff21
Sydney Anne Erickson <sydney@elixxir.io>
2022-09-02 11:27:33 -0700

    Update copyright headers

--- nonce/nonce.go
+++ nonce/nonce.go
@@ -1,8 +1,9 @@
-////////////////////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 xx network SEZC                                                       //
-//                                                                                        //
-// Use of this source code is governed by a license that can be found in the LICENSE file //
-////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
 
 // Package nonce contains our implementation of a nonce, including an expiration time,
 // generation time and TTL.
--- signature/rsa/rsa.go
+++ signature/rsa/rsa.go
@@ -1,8 +1,9 @@
-////////////////////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 xx network SEZC                                                       //
-//                                                                                        //
-// Use of this source code is governed by a license that can be found in the LICENSE file //
-////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
 
 // Package rsa includes wrappers to sign and verify the signatures of messages
 // with the PKCS#1 RSASSA-PSS signature algorithm:
